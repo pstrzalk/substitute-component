5e14ca701b57699a432bcfe4610aa53591700102
Paweł Strzałkowski <p.strzalkowski@visuality.pl>
2021-10-28 12:07:42 +0200

    Add interactive test

---
+++ test/interactive/run_service.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+require_relative './interactive_init'
+
+puts "Provide a service parameter (type - for nil)"
+service_parameter = gets.chomp.then do |str|
+  str == "-" ? nil : str.to_i
+end
+
+service = Service.build
+result = service.do_service_stuff(service_parameter)
+
+pp result
